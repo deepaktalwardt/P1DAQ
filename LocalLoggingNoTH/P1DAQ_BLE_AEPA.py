2e4af0a5d5698f9f3b3b7acd56fbc4bae6c4c902
Deepak Talwar <deepaktalwardt@berkeley.edu>
2016-02-11 01:08:14 -0800

    Added P1DAQ_BLE_AEPA.py

--- LocalLoggingNoTH/P1DAQ_BLE_AEPA.py
+++ LocalLoggingNoTH/P1DAQ_BLE_AEPA.py
@@ -110,7 +110,7 @@ while True:
     while (time.time() - startTime) < 2.5:
         data = sock.recv(1024)
         arr = ':'.join("{0:02x}".format(x) for x in data[12:6:-1])
-        devName = str(''.join("{0:02x}".format(x) for x in data[17:9:-1]))
+        devName = str(''.join("{0:02x}".format(x) for x in data[24:15:-1]))
         print(devName)
     #     if isRecDev(str(arr)):
     #         devID = str("".join("{0:02x}".format(x) for x in data[31:29:-1]))
