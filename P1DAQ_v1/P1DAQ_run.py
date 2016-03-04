49b2e3a1bae26992bc171ea6330721299effb522
deepaktalwardt <deepaktalwardt@berkeley.edu>
2016-03-04 20:58:40 +0000

    BLE working

--- P1DAQ_v1/P1DAQ_run.py
+++ P1DAQ_v1/P1DAQ_run.py
@@ -15,21 +15,22 @@ from ubidots import ApiClient
 # Variables
 
 # Functions
-def known_sensor():
-
-def get_sensor_reading():
-    to_return = 
-    data = get_BLE_raw():
-    BT_path = ':'.join("{0:02x}".format(x) for x in data[12:6:-1])
-    if known_sensor(str(BT_path)):
-        dev_id = str("".join("{0:02x}".format(x) for x in data[31:29:-1]))
-        num_conc = num_conc = int("".join("{0:02x}".format(x) for x in data[33:31:-1]),16)
-        mass_conc = int("".join("{0:02x}".format(x) for x in data[35:33:-1]),16)
-
+##def known_sensor():
+##    
+##def get_sensor_reading():
+##    to_return = 
+##    data = get_BLE_raw():
+##    BT_path = ':'.join("{0:02x}".format(x) for x in data[12:6:-1])
+##    if known_sensor(str(BT_path)):
+##        dev_id = str("".join("{0:02x}".format(x) for x in data[31:29:-1]))
+##        num_conc = num_conc = int("".join("{0:02x}".format(x) for x in data[33:31:-1]),16)
+##        mass_conc = int("".join("{0:02x}".format(x) for x in data[35:33:-1]),16)
+##
 
 
 # Loop to run
 while True:
-    data = get_BLE_data()
-    print(data)
-    time.sleep(1)
+    data = get_BLE_raw()
+    BT_path = ':'.join("{0:02x}".format(x) for x in data[12:6:-1])
+    print(BT_path)
+    
