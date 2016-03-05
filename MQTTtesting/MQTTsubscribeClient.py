a68197f6d7a1dee3a12f53c5c96df96836882315
Deepak Talwar <deepaktalwardt@berkeley.edu>
2016-03-04 19:19:59 -0800

    MQTTsubscribeClient updated

--- MQTTtesting/MQTTsubscribeClient.py
+++ MQTTtesting/MQTTsubscribeClient.py
@@ -1,17 +1,17 @@
 import paho.mqtt.client as paho
 import json
 
-DEVICE_TYPE 	= 	"P1"
-TOPIC 			=	"iot/SSRIOT/" + DEVICE_TYPE + "/#"
+DEVICE_TYPE     =   "P1"
+TOPIC           =   "iot/SSRIOT/" + DEVICE_TYPE + "/#"
 COUNTER = 1
 fieldnames = [dev_ids[0] + "_mc", dev_ids[0] + "_nc", 
-              dev_ids[1] + "_mc", dev_ids[1] + "_nc",
-              dev_ids[2] + "_mc", dev_ids[2] + "_nc",
-              dev_ids[3] + "_mc", dev_ids[3] + "_nc",
-              dev_ids[4] + "_mc", dev_ids[4] + "_nc",
-              "In Temp (deg C)", "Out Temp (deg C)",
-              "Relative Humidity (%)",
-              "Time (UT)"]
+			  dev_ids[1] + "_mc", dev_ids[1] + "_nc",
+			  dev_ids[2] + "_mc", dev_ids[2] + "_nc",
+			  dev_ids[3] + "_mc", dev_ids[3] + "_nc",
+			  dev_ids[4] + "_mc", dev_ids[4] + "_nc",
+			  "In Temp (deg C)", "Out Temp (deg C)",
+			  "Relative Humidity (%)",
+			  "Time (UT)"]
 
 folder_name = "/media/pi/Clarity/MQTTData/"
 num_file = len([f for f in os.listdir(folder_name)]) + 1
@@ -20,25 +20,25 @@ file_name = folder_name + "MQTTFile" + str(num_file) + ".csv"
 dict_to_write = dict.fromkeys(fieldnames)
 
 with open(file_name, "a") as csvfile:
-    writer = csv.DictWriter(csvfile, fieldnames = fieldnames)
-    writer.writeheader()
+	writer = csv.DictWriter(csvfile, fieldnames = fieldnames)
+	writer.writeheader()
 
 def on_subscribe(client, userdata, mid, granted_qos):
-    print("Subscribed: "+str(mid)+" "+str(granted_qos))
+	print("Subscribed: "+str(mid)+" "+str(granted_qos))
  
 def on_message(client, userdata, msg):
 	global COUNTER
 	global dict_to_write
-    print(msg.topic+" "+str(msg.qos)+" "+str(msg.payload))
-    jmsg = json.loads(msg.payload)
-    tn = jmsg['tn']
-    dev_id = tn[-4:]
-    num_conc = jmsg['d']['psd']['pm25num']
-    mass_conc = jmsg['d']['mc']['pm25conc']
-    int_temp = jmsg['d']['it']
-    out_temp = jmsg['d']['ot']
-    out_humi = jmsg['d']['oh']
-    time = jmsg['ts']
+	print(msg.topic+" "+str(msg.qos)+" "+str(msg.payload))
+	jmsg = json.loads(msg.payload)
+	tn = jmsg['tn']
+	dev_id = tn[-4:]
+	num_conc = jmsg['d']['psd']['pm25num']
+	mass_conc = jmsg['d']['mc']['pm25conc']
+	int_temp = jmsg['d']['it']
+	out_temp = jmsg['d']['ot']
+	out_humi = jmsg['d']['oh']
+	time = jmsg['ts']
 	dict_to_write[dev_id + '_mc'] = mass_conc
 	dict_to_write[dev_id + '_nc'] = num_conc
 	dict_to_write['Time (UT)'] = time
@@ -48,8 +48,8 @@ def on_message(client, userdata, msg):
 	COUNTER += 1
 	if COUNTER % 5 == 0:
 		with open(file_name, "a") as file_to_update:
-        updater = csv.DictWriter(file_to_update, fieldnames = fieldnames)
-        updater.writerow(dict_to_write)
+		updater = csv.DictWriter(file_to_update, fieldnames = fieldnames)
+		updater.writerow(dict_to_write)
 
 client = paho.Client(client_id = "p1", clean_session = True)
 client.on_subscribe = on_subscribe
