9b4a981d80bd649337f019db6a59b256d84b7cd9
Deepak Talwar <deepaktalwardt@berkeley.edu>
2016-03-14 16:28:10 -0700

    Added Client 2 support

--- P1DAQ_v1/command_client.py
+++ P1DAQ_v1/command_client.py
@@ -11,13 +11,13 @@ import time
 
 ORG_ID          =   "CLMTCO"
 DEVICE_TYPE     =   "P1"
-dev_id = "c1c8"
+dev_id = "002d"
 topic_sub = 'iot/SSRIOT/P1/' + dev_id
 topic_pub = "iot/SSRIOT/P1/" + dev_id
 tn = "d-" + ORG_ID + "-" + DEVICE_TYPE + "-" + dev_id
-sn = 100
-cid = 3
-cmd = 'set_clock' #
+#sn = 100
+#cid = 3
+#cmd = 'set_clock' #
 
 tz_test = ['+04:00',
 		   '+08:00',
@@ -63,7 +63,34 @@ client.loop_start()
 
 client.subscribe(topic_sub, qos=1)
 
-for tz in tz_test:
-	command = build_set_clock_command(tn, sn, cid, cmd, tz)
-	(rc, mid) = client.publish(topic_pub, command, qos=1)
-	time.sleep(6)
+def build_command(tn, sn, cid, cmd, arg):
+	# Initialize all JSONs
+    to_send = {}
+    c = {}
+
+    # Populate individual JSONs
+    c['tn'] = tn
+    c['sn'] =  sn
+    c['cid'] = cid
+    c['cmd'] = cmd
+
+    ts = datetime.datetime.now().isoformat()
+    arg = ts
+
+    c['arg'] = arg
+
+    to_send['c'] = c
+    to_send['ts'] = ts
+    return json.dumps(to_send)
+
+
+# for tz in tz_test:
+# 	command = build_set_clock_command(tn, sn, cid, cmd, tz)
+# 	(rc, mid) = client.publish(topic_pub, command, qos=1)
+# 	time.sleep(6)
+
+for st in st_test:
+	command = build_command(tn, 100, 1, 'set_st', st)
+	sleep_time = 2.1*st*2.5
+	print('Sleeping for: ' + str(sleep_time) + 'sec')
+	time.sleep(sleep_time)
