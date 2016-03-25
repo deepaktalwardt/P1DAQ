57025ec6a9783a11ed1e91a6cdb07ecb4ff0a067
deepaktalwardt <deepaktalwardt@berkeley.edu>
2016-03-25 01:05:23 -0700

    Fixed restart Fona

--- P1DAQ_v1/restart_fona.py
+++ P1DAQ_v1/restart_fona.py
@@ -1,7 +1,17 @@
 ## Restart fona ##
 import RPi.GPIO as GPIO
 import time
+import os
 
+def GPRS_off():
+    os.system('poff fona')
+    time.sleep(3)
+
+def GPRS_on():
+    os.system('pon fona')
+    time.sleep(5)
+
+GPRS_off()
 GPIO.setmode(GPIO.BCM)
 
 fona_key = 22
@@ -11,6 +21,9 @@ GPIO.output(fona_key, 0)
 time.sleep(2)
 
 GPIO.cleanup()
+time.sleep(10)
+GPRS_on()
 
-# print('Turning on GPRS')
-# GPRS_on()
+
+
+
